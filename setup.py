2a0ba18c968272060bd75e0022e1b91a3694cd61
cicd netengeu <netengeu@ticketmaster.eu>
2020-07-06 16:23:13 +0200

    update setup file

--- setup.py
+++ setup.py
@@ -1,12 +1,6 @@
 """setup.py file."""
 from setuptools import setup, find_packages
 
-with open("requirements.txt", "r") as fs:
-    reqs = [r for r in fs.read().splitlines() if (len(r) > 0 and not r.startswith("#"))]
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setup(
     name="tm_merger",
     version='0.0.1',
@@ -14,13 +8,8 @@ setup(
     author="Federico Olivieri",
     author_email="federico.olivieri@ticketmaster.nl",
     description="Ansible module for base and tenant config merger.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     classifiers=[
         'Topic :: Utilities',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
